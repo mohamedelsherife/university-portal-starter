cc093c8aa26da5b1519771529d121177dafaacf2
mohamed_elsherife <mohamed_5882@limu.edu.ly>
2026-06-17 12:24:33 +0200

    add dashboard in nav bar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,10 @@
 <header>
   <div class="logo"><a href="#"><i class="fa-solid fa-graduation-cap"></i> university portal</a></div>
   <nav>
-    <a href="{{ route('departments.index') }}"><i class="fa-regular fa-building"></i> Derpartment</a>
-    <a href="#"><i class="fa-solid fa-user-graduate"></i> Student</a>
-    <a href="#"><i class="fa-solid fa-book"></i> Course</a>
+    <a href="#"><i class="fa-solid fa-chart-column"></i> Dashboard</a>
+    <a href="{{ route('departments.index') }}"><i class="fa-regular fa-building"></i> Derpartments</a>
+    <a href="#"><i class="fa-solid fa-user-graduate"></i> Students</a>
+    <a href="#"><i class="fa-solid fa-book"></i> Courses</a>
 
         <div class="profile-dropdown">
       <input type="checkbox" id="profileToggle">
