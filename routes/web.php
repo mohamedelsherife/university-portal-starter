2b96d09c0128c50d5b8cb4138d28c7b80b403ec7
mohamed_elsherife <mohamed_5882@limu.edu.ly>
2026-06-27 02:06:29 +0200

    final changes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('welcome');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
